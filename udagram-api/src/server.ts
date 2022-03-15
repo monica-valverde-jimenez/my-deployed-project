929731bf29cefef6fc6e9ed3be001461bc8e156c
monica-valverde-jimenez <96742143+monica-valverde-jimenez@users.noreply.github.com>
2022-03-15 11:58:20 -0600

    fix: remove cors config

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -22,12 +22,12 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   app.use(bodyParser.json());
 
-  app.use(cors({ origin: false }));
+  app.use(cors());
 
   app.use("/api/v0/", IndexRouter);
 
   // Root URI call
-  app.get("/", cors({ origin: false }), async (req, res) => {
+  app.get("/", cors(), async (req, res) => {
     res.send("/api/v0/");
   });
 
