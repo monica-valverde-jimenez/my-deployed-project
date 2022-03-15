9aa4eeb2caab03d98b9a7d136bfc0ea4decce1fb
monica-valverde-jimenez <96742143+monica-valverde-jimenez@users.noreply.github.com>
2022-03-15 12:18:28 -0600

    Update server.ts

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -23,7 +23,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   app.use(bodyParser.json());
 
   app.use(cors());
-
+  app.options('*', cors()) // include before other routes
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', '*');
