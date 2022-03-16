9ef8f580df4e1fa350b0b384d2b133886b42af5c
monica-valverde-jimenez <96742143+monica-valverde-jimenez@users.noreply.github.com>
2022-03-15 18:49:50 -0600

    App

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: 'Udagram',
   //apiHost: 'http://localhost:8080/api/v0'
-  apiHost: "http://Myapi-env.eba-qemju7wj.us-east-1.elasticbeanstalk.com",
+  apiHost: "http://Myapi-env.eba-qemju7wj.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
